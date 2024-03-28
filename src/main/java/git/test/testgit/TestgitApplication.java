756ce4affd6fe8a2d26d7aad33fc7f5924d5151c
Artem Vlasov <baneslayer55@gmail.com>
2024-03-28 06:12:15 +0300

    init project

---
+++ src/main/java/git/test/testgit/TestgitApplication.java
@@ -0,0 +1,13 @@
+package git.test.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/test/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package git.test.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
