c2476ebb27a43e7d4702efcfeaf575d10196da76
Artem Vlasov <baneslayer55@gmail.com>
2024-03-28 19:34:38 +0300

    feature B2

--- src/main/java/git/test/testgit/TestgitApplication.java
+++ src/main/java/git/test/testgit/TestgitApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestgitApplication {
-
+	//featue B2
 	public static void main(String[] args) {
 		SpringApplication.run(TestgitApplication.class, args);
 	}
